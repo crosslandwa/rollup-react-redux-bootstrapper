bb8ea1e8896d239811d52c2565906499fc3066ac
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-05-13 23:34:16 +0100

    Functional linting

--- rollup.config.js
+++ rollup.config.js
@@ -38,8 +38,8 @@ module.exports = {
         'node_modules/react-is/index.js': [
           'isContextConsumer',
           'isValidElementType'
-        ],
+        ]
       }
-    }),
+    })
   ]
 }
