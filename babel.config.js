5ba979548ffc71bf9b7d9c79018bcc39938cedf6
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-12-10 21:29:51 +0000

    Update babel config to support async/await

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,5 @@
 
 module.exports = {
   exclude: 'node_modules/**',
-  presets: [ '@babel/preset-env', '@babel/preset-react' ]
+  presets: [['@babel/preset-env', { targets: { node: 10 } }], '@babel/preset-react']
 }
